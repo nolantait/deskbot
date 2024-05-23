a7845b122a04ead11e0016fda6a0e8fc8a10a9e3
Nolan J Tait <nolanjtait@gmail.com>
2024-05-23 01:46:37 -0700

    Applies rubocop lint

--- spec/deskbot_spec.rb
+++ spec/deskbot_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Deskbot do
   it "has a version number" do
-    expect(Deskbot::VERSION).not_to be nil
+    expect(Deskbot::VERSION).not_to be_nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(false).to be(true)
   end
 end
