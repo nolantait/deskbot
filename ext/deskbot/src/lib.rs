fa346453875eaf71319de5b0f7dc8699c70c95d1
Nolan J Tait <nolanjtait@gmail.com>
2024-05-22 23:49:06 -0700

    Adds basic typing with keyboard

--- ext/deskbot/src/lib.rs
+++ ext/deskbot/src/lib.rs
@@ -1,12 +1,30 @@
-use magnus::{function, prelude::*, Error, Ruby};
+use magnus::{function, prelude::*, Error, Ruby, Symbol};
+extern crate autopilot;
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+fn key_flag_from_symbol(symbol: Symbol) -> Option<autopilot::key::Flag> {
+    let name = symbol.name().unwrap().to_string();
+
+    match name.as_str() {
+        "shift" => Some(autopilot::key::Flag::Shift),
+        "control" => Some(autopilot::key::Flag::Control),
+        "alt" => Some(autopilot::key::Flag::Alt),
+        "meta" => Some(autopilot::key::Flag::Meta),
+        "help" => Some(autopilot::key::Flag::Help),
+        _ => None,
+    }
+}
+
+fn type_string(string: String, _flag: Symbol, wpm: f64, noise: f64) -> () {
+    if let Some(flag) = key_flag_from_symbol(_flag) {
+        autopilot::key::type_string(&string, &[flag], wpm, noise);
+    } else {
+        autopilot::key::type_string(&string, &[], wpm, noise);
+    }
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Deskbot")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    module.define_singleton_method("_type_string", function!(type_string, 4))?;
     Ok(())
 }
--- lib/deskbot.rb
+++ lib/deskbot.rb
@@ -6,4 +6,8 @@ require_relative "deskbot/deskbot"
 module Deskbot
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.type_string(text, flag: :none, wpm: 60.0, noise: 0.0)
+    _type_string(text, flag, wpm, noise)
+  end
 end
