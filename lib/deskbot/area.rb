70742c3dff4720ffb1e8b59126f4ba4903c709ae
Nolan J Tait <nolanjtait@gmail.com>
2024-05-24 16:53:51 -0700

    Applies rubocop lint

--- lib/deskbot/area.rb
+++ lib/deskbot/area.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Deskbot
   class Area < Dry::Struct
     attribute :x, Types::Float
--- lib/deskbot/bitmap.rb
+++ lib/deskbot/bitmap.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Deskbot
   class Bitmap
     def bounds
